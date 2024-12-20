26b40241eeb32e6fb2c4e70fbe4bb7def6ed5b5f
Mateus Marques <mateusmarques@gaudium.com.br>
2024-12-20 17:13:05 -0300

    fix: Test CI

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, CI/CD World! \n');
+  res.end('Hello, CI/CD World!  \n');
 });
 
 const port = process.env.PORT || 3000;
